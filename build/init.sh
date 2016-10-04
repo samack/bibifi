14b9932271a94114ecca1467fef2eb0db99aa214
spyd3rw3b <spyd3rw3b@gmail.com>
2016-10-03 21:44:49 -0500

    try wo sudo

--- build/init.sh
+++ build/init.sh
@@ -3,11 +3,11 @@
 if $(node -v | grep -Eq ^v4 ); then
     echo "Node installed"
 else
-    $(sudo apt-get install nodejs)
+    $(apt-get install nodejs)
 fi
 
 if $(npm -v | grep -Eq ^2); then
     echo "NPM installed...now check packages"
 else
-    $(sudo apt-get install npm)
+    $(apt-get install npm)
 fi
