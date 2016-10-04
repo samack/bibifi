7bca137cb444d9dc0361dbfd1646622caaee0b12
spyd3rw3b <spyd3rw3b@gmail.com>
2016-10-03 21:25:17 -0500

    split init and server start

---
+++ build/init.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if $(node -v | grep -Eq ^v4 ); then
+    echo "Node installed"
+else
+    $(sudo apt-get install nodejs)
+fi
+
+if $(npm -v | grep -Eq ^2); then
+    echo "NPM installed...now check packages"
+else
+    $(sudo apt-get install npm)
